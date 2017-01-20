a426bde3b3a351e6d983354edfa002fec2a074b6
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2017-01-20 22:15:20 +0600

    Reformat code

--- resources/assets/js/controllers/galleryController.js
+++ resources/assets/js/controllers/galleryController.js
@@ -1,64 +1,64 @@
-myApp.controller('galleryController', ['$scope', '$location', '$timeout', '$routeParams', '$log', 'galleryModel', 'Lightbox', function($scope, $location, $timeout, $routeParams, $log, galleryModel, Lightbox) {
-	angular.extend($scope, {
-		newGallery: {},
-		singleGallery: {},
-		dropzoneConfig: {
-			'options': { // passed into the Dropzone constructor
-				'url': baseUrl + 'upload-file'
-			},
-			'eventHandlers': {
-				'sending': function (file, xhr, formData) {
-					formData.append('_token', csrfToken);
-					formData.append('galleryID', $routeParams.galleryID);
-				},
-				'success': function (file, response) {
-				    $scope.singleGallery.images.push(response);
-				    $scope.$emit('imageAdded', $scope.singleGallery);
-				}
-			}
-		},
-		errorDiv: false,
-		errorMessages: []
-	});
+myApp.controller('galleryController', ['$scope', '$location', '$timeout', '$routeParams', '$log', 'galleryModel', 'Lightbox', function ($scope, $location, $timeout, $routeParams, $log, galleryModel, Lightbox) {
+    angular.extend($scope, {
+        newGallery: {},
+        singleGallery: {},
+        dropzoneConfig: {
+            'options': { // passed into the Dropzone constructor
+                'url': baseUrl + 'upload-file'
+            },
+            'eventHandlers': {
+                'sending': function (file, xhr, formData) {
+                    formData.append('_token', csrfToken);
+                    formData.append('galleryID', $routeParams.galleryID);
+                },
+                'success': function (file, response) {
+                    $scope.singleGallery.images.push(response);
+                    $scope.$emit('imageAdded', $scope.singleGallery);
+                }
+            }
+        },
+        errorDiv: false,
+        errorMessages: []
+    });
 
-	$scope.$on('imageAdded', function (event, args) {
+    $scope.$on('imageAdded', function (event, args) {
         $scope.singleGallery = args;
         $scope.$apply();
     });
 
-	galleryModel.getAllGalleries().then(function(successResponse) {
-		$scope.galleries = successResponse.data;
-		$scope.showGalleries = true;
-	});
+    galleryModel.getAllGalleries().then(function (successResponse) {
+        $scope.galleries = successResponse.data;
+        $scope.showGalleries = true;
+    });
 
-	if ($routeParams.galleryID) {
-		galleryModel.getGalleryById($routeParams.galleryID).then(function(successResponse) {
-			$scope.singleGallery = successResponse.data;
-			$scope.showGallery = true;
-		});
-	}
+    if ($routeParams.galleryID) {
+        galleryModel.getGalleryById($routeParams.galleryID).then(function (successResponse) {
+            $scope.singleGallery = successResponse.data;
+            $scope.showGallery = true;
+        });
+    }
 
-	angular.extend($scope, {
-		saveNewGallery: function(addGalleryForm) {
-			if (addGalleryForm.$valid) {
-				$scope.submitWithError = false;
-				galleryModel.saveGallery($scope.newGallery);
-			} else {
-				$scope.submitWithError = true;
-			}
-		},
+    angular.extend($scope, {
+        saveNewGallery: function (addGalleryForm) {
+            if (addGalleryForm.$valid) {
+                $scope.submitWithError = false;
+                galleryModel.saveGallery($scope.newGallery);
+            } else {
+                $scope.submitWithError = true;
+            }
+        },
 
-		viewGallery: function(galleryID) {
-			$location.path('/gallery/view/' + galleryID);
-		},
+        viewGallery: function (galleryID) {
+            $location.path('/gallery/view/' + galleryID);
+        },
 
-		deleteImage: function (imageId) {
-			var galleryId = $routeParams.galleryID;
-			galleryModel.deleteImage(galleryId, imageId);
+        deleteImage: function (imageId) {
+            var galleryId = $routeParams.galleryID;
+            galleryModel.deleteImage(galleryId, imageId);
         },
 
         openLightboxModal: function (index) {
             Lightbox.openModal($scope.singleGallery.images, index);
         }
-	});
+    });
 }]);
--- resources/assets/js/models/galleryModel.js
+++ resources/assets/js/models/galleryModel.js
@@ -1,37 +1,37 @@
-myApp.factory('galleryModel', ['$http', '$location', '$log', function($http, $location, $log) {
-	var galleryModel = {};
+myApp.factory('galleryModel', ['$http', '$location', '$log', function ($http, $location, $log) {
+    var galleryModel = {};
 
-	galleryModel.getAllGalleries = function() {
-		return $http.get(baseUrl + 'galleries');
-	};
+    galleryModel.getAllGalleries = function () {
+        return $http.get(baseUrl + 'galleries');
+    };
 
-	galleryModel.getGalleryById = function(galleryID) {
-		return $http.get(baseUrl + 'gallery/' + galleryID);
-	};
+    galleryModel.getGalleryById = function (galleryID) {
+        return $http.get(baseUrl + 'gallery/' + galleryID);
+    };
 
-	galleryModel.saveGallery = function(galleryData) {
-		$http({
-			method: 'POST',
-			url: baseUrl + 'galleries',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			data: {
-				name: galleryData.name
-			}
-		}).then(function(successResponse) {
-			$location.path('/gallery/view');
-		}, function(errorResponse) {
-			log.log(errorResponse)
-		});
-	};
-
-	galleryModel.deleteImage = function (galleryId, imageId) {
-		// $log.log(baseUrl + 'delete-image/' + imageId);
-		$http.delete(baseUrl + 'gallery/' + galleryId + '/delete-image/' + imageId).then(function (successResponse) {
-			$log.log(successResponse);
+    galleryModel.saveGallery = function (galleryData) {
+        $http({
+            method: 'POST',
+            url: baseUrl + 'galleries',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                name: galleryData.name
+            }
+        }).then(function (successResponse) {
+            $location.path('/gallery/view');
+        }, function (errorResponse) {
+            log.log(errorResponse)
         });
     };
 
-	return galleryModel;
+    galleryModel.deleteImage = function (galleryId, imageId) {
+        // $log.log(baseUrl + 'delete-image/' + imageId);
+        $http.delete(baseUrl + 'gallery/' + galleryId + '/delete-image/' + imageId).then(function (successResponse) {
+            $log.log(successResponse);
+        });
+    };
+
+    return galleryModel;
 }]);
