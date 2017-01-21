701309fb570c258e787ef50eb90717c96e9b3983
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2017-01-21 23:40:41 +0600

    Database backup scheduler

---
+++ app/Console/Commands/DatabaseBackup.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Storage;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class DatabaseBackup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Backup the database at current state';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $date = Carbon::now()->format('Y-m-d_h-i');
+        $user = env('DB_USERNAME');
+        $password = env('DB_PASSWORD');
+        $database = env('DB_DATABASE');
+        $command = "mysqldump --user={$user} -p{$password} {$database} > {$date}.sql";
+
+        $process = new Process($command);
+        $process->start();
+
+        while ($process->isRunning()) {
+            Storage::disk('local')->put("db_backup/{$date}.sql", file_get_contents("{$date}.sql"));
+            unlink("{$date}.sql");
+        }
+    }
+}
